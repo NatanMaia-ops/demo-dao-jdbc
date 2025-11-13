ba7f384a0bfffc644b1b35f87997f901e8351f22
NatanMaia-ops <natanmaia2005@gmail.com>
2025-11-13 13:32:49 -0300

    Creating project and git repository

---
+++ src/db/DbIntregityException.java
@@ -0,0 +1,14 @@
+package db;
+
+public class DbIntregityException extends RuntimeException{
+	private static final long serialVersionUID = 1L;
+
+	
+	
+	public DbIntregityException(String message) {
+		super(message);
+	}
+	
+	
+	
+}
